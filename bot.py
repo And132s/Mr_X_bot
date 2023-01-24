0f8700dca6776dafe84e9cc7593f21b9ec802e88
And132s <105209805+And132s@users.noreply.github.com>
2023-01-24 13:27:44 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,8 +40,8 @@ BOT_ID = bot_info.id
 
 # USE YOUR ROTATING PROXY API IN DICT FORMAT http://user:pass@providerhost:port
 proxies = {
-           'http': 'http://lelmcbsw-rotate:2060ugdxlk5z@p.webshare.io:80/',
-           'https': 'http://lelmcbsw-rotate:2060ugdxlk5z@p.webshare.io:80/'
+           'http': 'http://oxzkzsai-rotate:3mpjsx763utg@p.webshare.io:80/',
+           'https': 'http://oxzkzsai-rotate:3mpjsx763utg@p.webshare.io:80/'
 }
 
 session = requests.Session()
